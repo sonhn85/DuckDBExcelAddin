76cbcd83158f100ff55558c4eddfcc537376ffb9
sonhn85 <62049011+sonhn85@users.noreply.github.com>
2026-08-12 10:33:30 +0700

    Update DuckDBExcelAddin.h
    - Refactored and cleaned up the codebase for improved readability, maintainability, and consistency.
    - Improved internal documentation and source code comments.
    - Consolidated application configuration and constants into `config.h`.
    - Removed the `DUCKDB.SETSAMPLE()` worksheet function.
    - Added a `sample` named parameter to `xlrange()` for per-query type inference control.
    - Improved error reporting for statement parsing, preparation, execution, and result fetching failures.
    - Fixed a memory leak in xlrange() caused by duckdb_value objects not being destroyed during parameter binding.

--- DuckDBExcelAddin.h
+++ DuckDBExcelAddin.h
@@ -6,42 +6,36 @@
 #include "FRAMEWRK.H"
 
 #include "helper.h"
+#include "config.h"
 
-#define DUCKDB_DLL                      L"duckdb.dll"
-
-#define XLRANGE_DEFAULT_SAMPLE_COUNT    30
-
-#ifndef ADDIN_VERSION
-#define ADDIN_VERSION                   "dev"
-#endif
-
-// worksheet functions have 30 params by default
-// edit to add more
-#define PARAMS(X, Y) X(1) X(2) X(3) X(4) X(5) X(6) X(7) X(8) X(9) X(10) X(11) X(12) X(13) X(14) X(15) X(16) X(17) X(18) X(19) X(20)  X(21) X(22) X(23) X(24) X(25) X(26) X(27) X(28) X(29) Y(30)
-
-// do not change
-#define PARAMX(x) param##x
-#define PARAMX_SEP(x) param##x,
+/* Generate parameter names, declarations, help text,
+   and Excel type strings for worksheet functions. */
+#define WORKSHEET_PARAMX(x) param##x
+#define WORKSHEET_PARAMX_SEP(x) param##x,
 #define TYPE_SYMBOL(x) L"Q"
-#define PARAM_AND_TYPE(x) LPXLOPER12 PARAMX(x)
-#define PARAM_AND_TYPE_SEP(x) LPXLOPER12 PARAMX(x),
-#define PARAM_HELP(x) L"param"
-#define PARAM_HELP_SEP(x) L"param,"
-#define PARAM_LIST PARAMS(PARAMX_SEP, PARAMX)
-#define PARAMS_STR PARAMS(TYPE_SYMBOL, TYPE_SYMBOL)
-#define PARAM_AND_TYPE_LIST PARAMS(PARAM_AND_TYPE_SEP, PARAM_AND_TYPE)
-#define HELP_TEXT L"statements," PARAMS(PARAM_HELP_SEP, PARAM_HELP)
-#define TYPE_STR(X, Y) X PARAMS_STR Y
+#define WORKSHEET_PARAM_AND_TYPE(x) LPXLOPER12 WORKSHEET_PARAMX(x)
+#define WORKSHEET_PARAM_AND_TYPE_SEP(x) LPXLOPER12 WORKSHEET_PARAMX(x),
+#define WORKSHEET_PARAM_HELP(x) L"param"
+#define WORKSHEET_PARAM_HELP_SEP(x) L"param,"
+#define WORKSHEET_PARAM_LIST WORKSHEET_PARAMS(WORKSHEET_PARAMX_SEP, WORKSHEET_PARAMX)
+#define WORKSHEET_PARAM_STR WORKSHEET_PARAMS(TYPE_SYMBOL, TYPE_SYMBOL)
+#define WORKSHEET_PARAM_AND_TYPE_LIST WORKSHEET_PARAMS(WORKSHEET_PARAM_AND_TYPE_SEP, WORKSHEET_PARAM_AND_TYPE)
+#define HELP_TEXT L"statements," WORKSHEET_PARAMS(WORKSHEET_PARAM_HELP_SEP, WORKSHEET_PARAM_HELP)
+#define TYPE_STR(X, Y) X WORKSHEET_PARAM_STR Y
 
-// list of worksheet functions
-// remark: func name in dll, worksheet function name, type string, help text, function category
-#define FUNCTION_CATEGORY   L"DuckDB"
+/* Registered worksheet functions.
+   Arguments:
+     DLL function name,
+     Excel function name,
+     type string,
+     help text,
+     category. */
+
 #define XLL_FUNCS(X) \
-X(exec,             L"DUCKDB.EXEC",          TYPE_STR(L"QD%", L"$"),   HELP_TEXT,       FUNCTION_CATEGORY) \
+X(exec_sync,        L"DUCKDB.EXEC",          TYPE_STR(L"QD%", L"$"),   HELP_TEXT,       FUNCTION_CATEGORY) \
 X(exec_async,       L"DUCKDB.EXEC.ASYNC",    TYPE_STR(L">XD%", L"$"),  HELP_TEXT,       FUNCTION_CATEGORY) \
-X(query,            L"DUCKDB.QUERY",         TYPE_STR(L"QD%", L"$"),   HELP_TEXT,       FUNCTION_CATEGORY) \
+X(query_sync,       L"DUCKDB.QUERY",         TYPE_STR(L"QD%", L"$"),   HELP_TEXT,       FUNCTION_CATEGORY) \
 X(query_async,      L"DUCKDB.QUERY.ASYNC",   TYPE_STR(L">XD%", L"$"),  HELP_TEXT,       FUNCTION_CATEGORY) \
-X(set_sample_size,  L"DUCKDB.SETSAMPLE",     L"HH",                    L"sample_count", FUNCTION_CATEGORY) \
 X(addin_info,       L"DUCKDB.INFO",          L"Q",                     L"",             FUNCTION_CATEGORY)
 
 #ifdef __cplusplus
@@ -55,35 +49,27 @@ DLLEXPORT int WINAPI xlAutoClose(void);
 DLLEXPORT int WINAPI xlAutoRemove(void);
 DLLEXPORT void WINAPI xlAutoFree12(LPXLOPER12 pxFree);
 
-DLLEXPORT LPXLOPER12 WINAPI exec(
+DLLEXPORT LPXLOPER12 WINAPI exec_sync(
     const wchar_t *stmt,
-    PARAM_AND_TYPE_LIST
+    WORKSHEET_PARAM_AND_TYPE_LIST
 );
 
 DLLEXPORT void WINAPI exec_async(
     LPXLOPER12 asyncHandle,
     const wchar_t *stmt,
-    PARAM_AND_TYPE_LIST
+    WORKSHEET_PARAM_AND_TYPE_LIST
 );
 
-DLLEXPORT LPXLOPER12 WINAPI query(
+DLLEXPORT LPXLOPER12 WINAPI query_sync(
     const wchar_t *stmt,
-    PARAM_AND_TYPE_LIST
+    WORKSHEET_PARAM_AND_TYPE_LIST
 );
 
 DLLEXPORT void WINAPI query_async(
     LPXLOPER12 asyncHandle,
     const wchar_t *stmt,
-    PARAM_AND_TYPE_LIST
+    WORKSHEET_PARAM_AND_TYPE_LIST
 );
-
-DLLEXPORT void WINAPI query_async(
-    LPXLOPER12 asyncHandle,
-    const wchar_t *stmt,
-    PARAM_AND_TYPE_LIST
-);
-
-DLLEXPORT unsigned short set_sample_size(unsigned short nsample);
 
 DLLEXPORT LPXLOPER12 addin_info(void);
 
